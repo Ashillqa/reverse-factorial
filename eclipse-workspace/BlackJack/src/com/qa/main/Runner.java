a90e86e042dd363c9dfe37b2ad59a27a3b20923d
= <ashillpathak@gmail.com>
2020-03-25 10:46:17 +0000

    exercises

---
+++ eclipse-workspace/BlackJack/src/com/qa/main/Runner.java
@@ -0,0 +1,14 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		BlackJack test1 = new BlackJack();
+		
+		System.out.println("BlackJack test run: ");
+		System.out.println(test1.Game(21,20));
+
+	}
+
+}
